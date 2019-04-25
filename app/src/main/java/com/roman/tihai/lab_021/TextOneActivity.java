fea11fa2d0c9a415f5ce2c5a31584d0c9f7e8234
Rtihai <rtihai@gmail.com>
2019-04-24 21:17:07 -0700

    Lab 02.1 + Challenge

---
+++ app/src/main/java/com/roman/tihai/lab_021/TextOneActivity.java
@@ -0,0 +1,14 @@
+package com.roman.tihai.lab_021;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text1);
+
+    }
+}
---
+++ app/src/main/java/com/roman/tihai/lab_021/TextTwoActivity.java
@@ -0,0 +1,13 @@
+package com.roman.tihai.lab_021;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text2);
+    }
+}
